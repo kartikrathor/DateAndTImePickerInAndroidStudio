76fa0e67be7437fa30d12d400c3c24001d138c88
Kartik rathor <60212068+kartikrathor@users.noreply.github.com>
2021-03-11 14:05:56 +0530

    Update DatePickerActivity.kt

--- DatePickerActivity.kt
+++ DatePickerActivity.kt
@@ -1,4 +1,20 @@
+package com.example.navigationdraweractivity
 
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class HomeActivity : AppCompatActivity() {
 
